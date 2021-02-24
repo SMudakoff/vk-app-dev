81ae4c1755fbe9c89689d70bc3a839ad35517578
Ushakov Sergey <serega_twitch@mail.ru>
2021-02-25 01:27:09 +0300

    Updates

---
+++ precache-manifest.fcdf8f1ee6d78e7fda6273e5cd236119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aadc9a1354b3deddf5f4",
+    "url": "./static/css/main.0d34ecf8.chunk.css"
+  },
+  {
+    "revision": "aadc9a1354b3deddf5f4",
+    "url": "./static/js/main.b45665bc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b4c8dc37592f3873d663",
+    "url": "./static/css/2.faf0fcf7.chunk.css"
+  },
+  {
+    "revision": "b4c8dc37592f3873d663",
+    "url": "./static/js/2.c4d4e59a.chunk.js"
+  },
+  {
+    "revision": "1cc6e450479d80cb46fb834be8e39bbd",
+    "url": "./index.html"
+  }
+];
